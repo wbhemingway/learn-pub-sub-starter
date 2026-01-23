01f491088855cc77bf7d05c1d411a6b355ab2652
wbhemingway <william.b.hemingway@gmail.com>
2026-01-23 18:10:35 -0500

    added json publishing

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -18,6 +20,14 @@ func main() {
 
 	defer rmqConn.Close()
 	fmt.Printf("Connection to RMQ adress %s was successful!\n", rmqAddr)
+
+	amqpChann, err := rmqConn.Channel()
+	if err != nil {
+		log.Fatalf("Could not make RMQ cchannel: %v", err)
+	}
+
+	pubsub.PublishJSON(amqpChann, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+
 	fmt.Println("Starting Peril server...")
 
 	signalChan := make(chan os.Signal, 1)
---
+++ internal/pubsub/json.go
@@ -0,0 +1,21 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	jsonData, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+
+	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{ContentType: "application/json", Body: jsonData})
+	if err != nil {
+		return err
+	}
+	return nil
+}
